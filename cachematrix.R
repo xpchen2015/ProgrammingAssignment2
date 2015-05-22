0a3dd2edf9e1916b7067b472ef3bf1b35da334db
Xiaopei <xpchen125@hotmail.com>
2015-05-22 09:48:33 -0400

    Xiaopei Chen

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions are used to crate a special object that stores a matrix 
+##and cache's its inverse.
+## If global and permanent assignments are intended within a function, 
+##then either the "superassignment" operator, <<- or 
+##the function assign() can be used. 
 
-## Write a short comment describing this function--Xiaopei
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        IM<- NULL
+        setM <- function (y){
+                x<<-y
+                IM<<-NULL
+        }
+        getM <- function() x
+        setIM <- function(INV) IM <<- INV
+        getIM <- function() IM
+        list(setM = setM, getM=getM,
+             setIM = setIM, getIM = getIM)
+        
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix function. 
+#If the inverse has already been calculated, 
+##then the cachesolve function should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        IM <- x$getIM()
+        if(!is.null(IM)){
+                message ("getting cached matrix")
+                return(IM)
+        }
+        data <- x$getM()
+        IM <- solve(data)
+        x$setIM(IM)
+        IM
 }
